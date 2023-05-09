4f6038bcb1b42be0ce2699c6cb035f0da7e2c5aa
Ankit3002 <hmankit2003@gmail.com>
2023-05-09 12:40:10 +0530

    added: pappl build with guten printer app file

---
+++ Build_pappl_guten_printer.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+sudo apt-get update
+sudo apt-get install autopoint
+sudo apt-get install libtool
+sudo apt-get install cups-dev
+sudo apt install libppd-dev
+sudo apt install libcupsfilters-dev
+
