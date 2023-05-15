61e69c28cd8d97e38d4d7bfe2b86c6640667a442
Ankit3002 <hmankit2003@gmail.com>
2023-05-16 00:45:45 +0530

    Added: repo

---
+++ clone_repo.sh
@@ -0,0 +1,6 @@
+#! /usr/bash/bin
+
+
+git clone https://github.com/OpenPrinting/cups.git
+git clone https://github.com/michaelrsweet/pappl.git
+git clone https://github.com/OpenPrinting/pappl-retrofit.git
